254acf908f7ddf7f5117128d79d3e3a916cc5366
QDSG <piftao1127@163.com>
2019-10-14 18:33:13 +0800

    Array Fliter Closure And 泛型 Element

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -15,17 +15,7 @@ struct Concentration {
     
     private var indexOfTheOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -59,5 +49,11 @@ struct Concentration {
         }
         
         // TODO: 洗牌
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
