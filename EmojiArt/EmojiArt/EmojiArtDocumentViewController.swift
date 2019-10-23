b38264ce18553b546726e486fb682e1b8759bb93
QDSG <piftao1127@163.com>
2019-10-23 17:01:58 +0800

    UITableView

---
+++ EmojiArt/EmojiArt/EmojiArtDocumentViewController.swift
@@ -0,0 +1,49 @@
+//
+//  EmojiArtDocumentViewController.swift
+//  EmojiArt
+//
+//  Created by QDSG on 2019/10/23.
+//  Copyright © 2019 tao Tao. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtDocumentViewController: UITableViewController {
+
+    var emojiArtDocuments = ["One", "Two", "Three"]
+    
+    // MARK: - Action
+    @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension EmojiArtDocumentViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArtDocuments.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
+        cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            emojiArtDocuments.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension EmojiArtDocumentViewController {
+    
+}
--- EmojiArt/EmojiArt/Utilities.swift
+++ EmojiArt/EmojiArt/Utilities.swift
@@ -138,3 +138,15 @@ extension UIImage {
         return image
     }
 }
+
+extension String {
+    func madeUnique(withRespectTo otherStrings: [String]) -> String {
+        var possiblyUnique = self
+        var uniqueNumber = 1
+        while otherStrings.contains(possiblyUnique) {
+            possiblyUnique = self + "\(uniqueNumber)"
+            uniqueNumber += 1
+        }
+        return possiblyUnique
+    }
+}
